7d8cc8cc1e199b6640fcb342913e9e1e3204f89d
Mac Kwan <info@mackwan.net>
2017-08-24 16:10:25 +0800

    Draft implementation of the Alipay authentication.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # passport-alipay
 
+Passport strategy for authenticating with Alipay
+
 支付宝生活号OAuth 2.0授权
---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Module dependencies.
+ */
+var Strategy = require('./lib/strategy');
+
+/**
+ * Expose `Strategy` directly from package.
+ */
+exports = module.exports = Strategy;
+
+/**
+ * Export constructors.
+ */
+exports.Strategy = Strategy;
